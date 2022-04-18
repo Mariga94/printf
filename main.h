436cda45981430b9d4b42f12ba4f87c3aa6c5750
Joseph Mariga <jsphmariga@gmail.com>
2022-04-18 15:43:24 +0000

    Add conversion specifiers

---
+++ main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * struct op - struct up
+ *
+ * @op: The operator
+ * @f: The function associated
+ */
+typeof struct conversion_specifier
+{
+	char *csp;
+	int (*f)(va_list);
+} conv_s;
+int _printf(const char *format, ...);
+#endif
