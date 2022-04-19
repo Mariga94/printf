7ca103ac67db704f45ea62f9a5a173ea2c5f5b9d
Joseph Mariga <jsphmariga@gmail.com>
2022-04-19 23:28:21 +0000

    Add curly braces to statements

--- _printf.c
+++ _printf.c
@@ -20,26 +20,33 @@ int _printf(const char *format, ...)
 	while (format[i] != '\0')
 	{
 		if (format[i] != '%')
+		{
 			_putchar(format[i]);
 			display++;
+		}
 		else
+		{
 			if (format[i + 1] == 'c')
+			{
 				i++;
-				c = va_arg(args, int);
-				_putchar(c);
+				_putchar(va_arg(args, int));
 				display++;
-
+			}
 			else if (format[i + 1] == 's')
+			{
 				i++;
 				s = va_arg(args, char *);
 				for (j = 0; s[j] != '\0'; j++)
 					_putchar(s[j]);
 					display++;
-
+			}
 			else if (format[i + 1] == '%')
+			{
 				i++;
 				_putchar('%');
 				display++;
+			}
+		}
 		i++;
 	}
 	va_end(args);
